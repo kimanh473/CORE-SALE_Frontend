2648d8ac5a4f34b9e2003b13cab53ec316c0eb31
Tran Quang Minh <cntt.dev04@btpholdings.com>
2024-01-16 17:36:00 +0700

    update

--- src/page/products/ListProduct.vue
+++ src/page/products/ListProduct.vue
@@ -33,12 +33,22 @@
         class="!p-[10px]"
         :columns="columns"
         :data-source="listProduct"
+        :pagination="false"
+        v-model:current="current"
         bordered
         row-key="id"
         ><template #bodyCell="{ column, record }">
           <template v-if="column.key === 'image'">
-            <div v-for="(item, index) in record.image" :key="index">
-              <img :src="UriImg + '/' + item" alt="" width="100" height="100" />
+            <img
+              :src="UriImg + '/' + record.image[0]"
+              alt=""
+              width="20"
+              height="40"
+            />
+          </template>
+          <template v-if="column.key === 'web_site_code'">
+            <div v-for="(item, index) in record.web_site_code" :key="index">
+              {{ formatWeb(item) }}
             </div>
           </template>
           <template v-if="column.key === 'id'">
@@ -50,8 +60,15 @@
         </template>
         <template #switch="{ text }">
           <a-switch v-model:checked="isCheck" />
-        </template> </a-table
-    ></template>
+        </template>
+      </a-table>
+      <a-pagination
+        v-model:current="current"
+        v-model:pageSize="perPage"
+        show-quick-jumper
+        :total="totalPage"
+        @change="changePage"
+    /></template>
 
     <template v-slot:footer>footer</template>
   </base-layout>
@@ -66,23 +83,41 @@
 </template>
 
 <script setup lang="ts">
-  import BaseLayout from '../../layout/baseLayout.vue'
-  import SideBar from '../../components/common/SideBar.vue'
-  import Header from '../../components/common/Header.vue'
+  import BaseLayout from '@/layout/baseLayout.vue'
+  import SideBar from '@/components/common/SideBar.vue'
+  import Header from '@/components/common/Header.vue'
   import { useRoute, useRouter } from 'vue-router'
-  import { ref, reactive } from 'vue'
+  import { ref, reactive, computed } from 'vue'
   import { useToast } from 'vue-toastification'
-  import { useProduct } from '../../store/modules/store-setting/products'
+  import { useProduct } from '@/store/modules/store-setting/products'
+
   //   import { storeToRefs } from 'pinia'
-  import ModalDelete from '../../components/modal/ModalConfirmDelelte.vue'
+  import ModalDelete from '@/components/modal/ModalConfirmDelelte.vue'
+  import { useWebCatalog } from '@/store/modules/web-catalog/webcatalog'
   import { storeToRefs } from 'pinia'
   const route = useRoute()
   const router = useRouter()
   const toast = useToast()
+  const dataWebsite = useWebCatalog()
+  dataWebsite.getAllWebCatalogAction()
+  const { listWeb } = storeToRefs(dataWebsite)
+  function formatWeb(webcode: string) {
+    let webName = listWeb.value.find((item: any) => item.code == webcode)
+    return webName?.web_name
+  }
+  const EndTimeLoading = () => {
+    isLoading.value = false
+  }
+  const current = ref(1)
+  const perPage = ref(10)
   const dataProduct = useProduct()
-  dataProduct.getListProductAction(4, 1)
-  const { listProduct } = storeToRefs(dataProduct)
-  console.log(listProduct)
+  dataProduct.getListProductAction(perPage.value, current.value, EndTimeLoading)
+  const changePage = (pageNumber: number) => {
+    isLoading.value = true
+    dataProduct.getListProductAction(perPage.value, pageNumber, EndTimeLoading)
+  }
+  const { listProduct, totalPage } = storeToRefs(dataProduct)
+  console.log(totalPage)
   const isCheck = ref<boolean>(false)
   const isLoading = ref<boolean>(false)
   const isOpenConfirm = ref<boolean>(false)
@@ -114,11 +149,12 @@
     },
     {
       title: 'Trạng thái',
-      dataIndex: 'attribute_set_id',
+      dataIndex: 'status',
     },
     {
       title: 'Website',
-      dataIndex: 'attribute_set_id',
+      dataIndex: 'web_site_code',
+      key: 'web_site_code',
     },
     {
       title: 'Giá niêm yết',
@@ -143,9 +179,7 @@
   const handleCloseConfirm = () => {
     isOpenConfirm.value = false
   }
-  const EndTimeLoading = () => {
-    isLoading.value = false
-  }
+
   const CreateProduct = () => {
     router.push('/create-product')
   }
